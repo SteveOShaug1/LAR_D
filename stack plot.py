eeff4d936d10e524ab51cb79c6120f6b3f87a3b7
Steve <smarterclowns@gmail.com>
2024-10-19 06:05:51 -0700

    Add files via upload

---
+++ stack plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('_mpl-gallery')
+
+# make data
+x = np.arange(0, 10, 2)
+ay = [1, 1.25, 2, 2.75, 30]
+by = [1, 1, 1, 1, 1]
+cy = [2, 1, 2, 1, 2]
+y = np.vstack([ay, by, cy])
+
+# plot
+fig, ax = plt.subplots()
+
+ax.stackplot(x, y)
+
+ax.set(xlim=(0, 30), xticks=np.arange(1, 30),
+       ylim=(0, 30), yticks=np.arange(1, 30))
+
+plt.show()
